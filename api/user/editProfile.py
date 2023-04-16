a8ccd18d058e9d1db033e63f3b0975f6dce8d7cb
Piyaphat Boontham <77188130+dunkdink@users.noreply.github.com>
2023-04-16 14:52:36 +0700

    improve editprofile

--- api/user/editProfile.py
+++ api/user/editProfile.py
@@ -8,8 +8,8 @@ router = APIRouter()
 __pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-@router.put("/user")
-def updateProfile(username: str, name: str, age: int, gender: str):
+@router.put("/user/profile")
+def updateProfile(username: str, name: str, email: str, age: int, gender: str):
     user = db.session.query(User).filter(User.username == username).first()
 
     if user is None:
@@ -21,7 +21,11 @@ def updateProfile(username: str, name: str, age: int, gender: str):
     if not name:
         raise HTTPException(status_code=400, detail="Name is required")
 
+    if not email or "@" not in email:
+        raise HTTPException(status_code=400, detail="Invalid email")
+
     user.name = name
+    user.email = email
     user.age = age
     user.gender = gender
 
@@ -30,8 +34,9 @@ def updateProfile(username: str, name: str, age: int, gender: str):
     return user
 
 
+
 @router.put("/user/password")
-def updatePassword(username: str, old_password: str, new_password: str):
+def updatePassword(username: str, old_password: str, new_password: str, confirm_password: str):
     user = db.session.query(User).filter(User.username == username).first()
 
     if user is None:
@@ -43,11 +48,19 @@ def updatePassword(username: str, old_password: str, new_password: str):
     if not new_password:
         raise HTTPException(status_code=400, detail="New password is required")
 
+    if new_password != confirm_password:
+        raise HTTPException(
+            status_code=400, detail="New password and confirm password do not match")
+
     if not __pwd_context.verify(old_password, user.password):
         raise HTTPException(status_code=400, detail="Incorrect old password")
+
+    if len(new_password) < 8 or new_password.isdigit() or new_password.isalpha():
+        raise HTTPException(status_code=400, detail="Password is too weak")
 
     user.password = __pwd_context.hash(new_password)
 
     db.session.commit()
 
     return user
+
